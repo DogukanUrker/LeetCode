f51f1216d3bcd318e45b07b1dd34f6cca7c9d7b8
Doğukan Ürker <dogukanurker@icloud.com>
2023-06-10 18:04:14 +0300

    Time: 65 ms (42.18%), Space: 17.2 MB (7.27%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,6 +1,18 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        nums.append(target)
-        nums.sort()
-        return nums.index(target)
+        match target in nums:
+            case True:
+                return nums.index(target)
+            case _:
+                for i in range(len(nums)):
+                    if nums[i] > target:
+                        return i 
+                return len(nums)  
+
+    
+        
+            
+
+              
+            
         
